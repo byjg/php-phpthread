74dfaea9eba22988d3d0702d324271ead9e20a7d
Joao Gilberto Magalhaes <joao@byjg.com.br>
2015-01-09 09:11:15 -0200

    Added comments. minor improvements and fixed some bugs.

--- ByJG/PHPThread/Thread.php
+++ ByJG/PHPThread/Thread.php
@@ -2,33 +2,65 @@
 
 namespace ByJG\PHPThread;
 
-use Exception;
+use InvalidArgumentException;
+use RuntimeException;
 
+/**
+ * Native Implementation of Threads in PHP.
+ *
+ * A class to spawn a thread. Only works in *nix environments,
+ * as Windows platform is missing libpcntl.
+ *
+ * Forks the process.
+ */
 class Thread
 {
-    public function __construct($callback)
-    {
-        if (!function_exists('pcntl_fork'))
-            throw new Exception('PHP was compiled without --enable-pcntl or you are running on Windows.');
 
-        if ($callback == null)
-			throw new Exception('The callback function is required.');
+	/**
+	 * Class constructor
+	 *
+	 * @param mixed $callback string with the function name or a array with the instance and the method name
+	 * @throws RuntimeException
+	 * @throws InvalidArgumentException
+	 */
+	public function __construct($callback)
+	{
+		if (!function_exists('pcntl_fork'))
+			throw new RuntimeException('PHP was compiled without --enable-pcntl or you are running on Windows.');
+
+		if ($callback == null)
+			throw new InvalidArgumentException('The callback function is required.');
 
 		$this->setCallback($callback);
-    }
+	}
 
-    public function isAlive()
-    {
-        return (pcntl_waitpid($this->_pid, $status, WNOHANG) === 0);
-    }
+	/**
+	 * Check if the forked process is alive
+	 * @return bool
+	 */
+	public function isAlive()
+	{
+		return (pcntl_waitpid($this->_pid, $status, WNOHANG) === 0);
+	}
 
-    protected function setCallback($callback)
-    {
-		if (is_array($callback)) {
-			if ((count($callback) == 2) && method_exists($callback[0], $callback[1])  && is_callable($callback))
+	/**
+	 * Private function for set the method will be forked;
+	 *
+	 * @param string $callback string with the function name or a array with the instance and the method name
+	 * @throws InvalidArgumentException
+	 */
+	protected function setCallback($callback)
+	{
+		if (is_array($callback))
+		{
+			if ((count($callback) == 2) && method_exists($callback[0], $callback[1]) && is_callable($callback))
+			{
 				$this->_callback = $callback;
+			}
 			elseif (count($callback) != 2)
-				throw new Exception("The parameter need to be a two elements array with a instance and a method of this instance or just a PHP static function");
+			{
+				throw new InvalidArgumentException("The parameter need to be a two elements array with a instance and a method of this instance or just a PHP static function");
+			}
 			else
 			{
 				if (is_object($callback[0]))
@@ -39,53 +71,68 @@ class Thread
 				{
 					$className = $callback[0];
 				}
-				throw new Exception("The method " . $className . "->". $callback[1] . "() does not exists or not is callable");
+				throw new InvalidArgumentException("The method " . $className . "->" . $callback[1] . "() does not exists or not is callable");
 			}
 		}
 		elseif (function_exists($callback) && is_callable($callback))
 			$this->_callback = $callback;
 		else
-			throw new Exception("$callback is not valid function");
-    }
+			throw new InvalidArgumentException("$callback is not valid function");
+	}
 
-    public function start()
-    {
-        if (($this->_pid = pcntl_fork()) == -1)
-            throw new Exception('Couldn\'t fork the process');
-
-        if (!$this->_pid) {
-            // Child.
-
-            pcntl_signal(SIGTERM, array($this, 'SignalHandler'));
-
-            $args = func_get_args();
-
-            !empty($args) ? call_user_func_array($this->_callback, $args) : call_user_func($this->_callback);
-
-            exit(0);
-        }
-
-        // Parent.
-    }
-
-    public function stop($signal = SIGKILL, $wait = false)
-    {
-        if ($this->IsAlive())
+	/**
+	 *
+	 * @throws RuntimeException
+	 */
+	public function start()
+	{
+		if (($this->_pid = pcntl_fork()) == -1)
 		{
-            posix_kill($this->_pid, $signal);
+			throw new RuntimeException('Couldn\'t fork the process');
+		}
 
-	        if ($wait)
+		if (!$this->_pid)
+		{
+			// Child.
+			pcntl_signal(SIGTERM, array($this, 'signalHandler'));
+			$args = func_get_args();
+			!empty($args) ? call_user_func_array($this->_callback, $args) : call_user_func($this->_callback);
+
+			exit(0);
+		}
+
+		// Parent.
+	}
+
+	/**
+	 *
+	 * @param type $signal
+	 * @param type $wait
+	 */
+	public function stop($signal = SIGKILL, $wait = false)
+	{
+		if ($this->isAlive())
+		{
+			posix_kill($this->_pid, $signal);
+
+			if ($wait)
 				pcntl_waitpid($this->_pid, $status);
-        }
-    }
+		}
+	}
 
-    private function signalHandler($signal)
-    {
-        switch ($signal) {
-            case SIGTERM:
-                exit(0);
-        }
-    }
+	/**
+	 *
+	 * @param type $signal
+	 */
+	private function signalHandler($signal)
+	{
+		switch ($signal)
+		{
+			case SIGTERM:
+				exit(0);
+		}
+	}
 
-    private $_callback, $_pid;
+	private $_callback, $_pid;
+
 }
